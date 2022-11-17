4c707de7de957d14fd1227eb71b7c09452d5e467
Geetanshu Garg <geetanshugarg39@gmail.com>
2022-11-17 18:13:20 +0530

    Designed the footer section

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-// import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
 import Home from './components/Home';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
     <Navbar/>
     <Home/>
+    <Footer/>
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import "./Footer.css"
+function Footer() {
+  return (
+    <>
+    <div className='Box'>
+      
+      <div className='Element1'>
+          <h3>ABOUT GENEXP</h3>
+          <p><b>Vision:</b> We aim to provide common platform for all your internship/placement related work.<br/><b>Product: </b>We designed our website in easy to use way with concern of personal data protection using blockchain technology.</p>
+          <div></div>
+      </div>
+        
+      <div className='Element'>
+          <h3>Quick Links</h3>
+          <a href='\'>Login<br/></a>
+          <a href='\'>Start Hiring<br/></a>
+          <a href='\'>Apply<br/></a>
+          <a href='\'>Job Openings<br/></a>
+      </div>
+      <div className='Element'>
+          <h3>Resources</h3>
+          <a href='\'>Security<br/></a>
+          <a href='\'>CDGC<br/></a>
+          <a href='\'>PEC<br/></a>
+          <a href='\'>About Us<br/></a>
+      </div>
+      
+  </div>
+</>
+      )
+}
+
+export default Footer

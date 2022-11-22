5b2663510ebe6e4d96520367a16dd2403884d39a
anshRS <anshraj0426@gmail.com>
2022-11-22 17:01:14 +0530

    Added routes to dashboard components

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Security from './components/Security';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
 import AboutUs from './components/AboutUs';
+import DashHome from './components/DashHome';
+import Offers from './components/Offers';
 import { useState } from 'react';
 
 import {
@@ -33,6 +35,8 @@ function App() {
           <Route exact path="/signup" element={<SignUp funcNav={setShowNav}/>} />
           <Route exact path="/login" element={<Login funcNav={setShowNav}/>} />
           <Route exact path="/aboutus" element={<AboutUs funcNav={setShowNav}/>} />
+          <Route exact path="/dashboard" element={<DashHome funcNav={setShowNav}/>} />
+          <Route exact path="/offers" element={<Offers funcNav={setShowNav}/>} />
         </Routes>
 
         {showNav &&

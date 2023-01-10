81ca7a94d1f42d54725931b1a76b86d7a2497e2a
anshRS <anshraj0426@gmail.com>
2023-01-10 16:33:29 +0530

    Handled the errors to display in Signup Frontend

--- src/components/SignUp.js
+++ src/components/SignUp.js
@@ -1,50 +1,61 @@
 import React from 'react'
-// import { Link } from "react-router-dom";
-// import { HashLink } from 'react-router-hash-link';
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faGoogle } from "@fortawesome/free-brands-svg-icons";
+// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+// import { faGoogle } from "@fortawesome/free-brands-svg-icons";
 import './SignUp.css';
-import { Link, useLocation, useNavigate } from "react-router-dom";
-import { useEffect } from 'react';
-import { useState } from 'react';
-import {useSignupMutation} from '../services/authApi';
+import { useLocation, useNavigate } from "react-router-dom";
+import { useEffect, useState } from 'react';
+import { useSignupMutation } from '../services/authApi';
 
 export default function SignUp(props) {
     props.funcNav(true);
-    useEffect(() => { document.title = "GenExp | Sign Up"; }, []);
-    const { pathname } = useLocation();
+    useEffect(() => { 
+        document.title = "GenExp | Sign Up"; 
+    }, []);
 
+    const { pathname } = useLocation();
     useEffect(() => {
         window.scrollTo(0, 0);
     }, [pathname]);
-
+    
     const [fullname,setFullName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [password2, setcPassword] = useState("");
-    const [signup, {isLoading, isSuccess}] = useSignupMutation();
-    const navigate = useNavigate();
+    const [signup] = useSignupMutation();
+    const [signuperror, setSignUpError] = useState({
+        passwordError: "",
+        usernameError: "",
+        emailError: ""
 
+    })
+    const navigate = useNavigate();
     
     const submitForm = async (e) =>{
         e.preventDefault();
-        console.log(fullname)
-        console.log(email)
-        console.log(password)
-        console.log(password2)
-        
-        
         const newEntry = {
             username:fullname, 
             email:email, 
             password:password, 
             password2:password2
         }
+        const response = await signup(newEntry)  
+        
+        if(response.data.emailExist){
+            setSignUpError({emailError: "You already have an account with this email ID. Please use another email"})
+        }
+        
+        if(response.data.usernameExist){
+            setSignUpError({usernameError: "Username already exists. Please enter different username"})
+        }
 
-        const res = await signup(newEntry)
-        navigate('/dashboard')
+        if(password !== password2){            
+            setSignUpError({passwordError:"Password and Confirm Password not matching"})
+        }
+        
+        if(response.data.token){            
+            navigate('/dashboard')            
+        }        
     }
-
     
   return (
     <>
@@ -52,13 +63,11 @@ export default function SignUp(props) {
         <div className="signup-heading">
         <h1>SIGN UP</h1>
         </div>
-        <div className="signup-box">
-            
-            
+        <div className="signup-box">  
+            <h3>Welcome To GENEXP!</h3>
             <form name='signUpForm' method='' action="" onSubmit={submitForm}>
                 <div className="inputBox">                    
-                    <input type="text" name='fullname' id = 'sname' value = {fullname} onChange = {(e) => setFullName(e.target.value)} autoComplete='off' required placeholder='Enter Your Username'/ >
-
+                    <input type="text" name='fullname' id = 'sname' value = {fullname} onChange = {(e) => setFullName(e.target.value)} autoComplete='off' required placeholder='Enter Username'/ >
                 </div>
                 <div className="inputBox">                    
                     <input type="email" name='email' id = 'semail' value = {email} onChange = {(e) => setEmail(e.target.value)} autoComplete='off' required placeholder='Email'/>
@@ -69,28 +78,16 @@ export default function SignUp(props) {
                 <div className="inputBox">                    
                     <input type="password" name='cpassword' id = 'scpassword' value = {password2} onChange = {(e) => setcPassword(e.target.value)} autoComplete='off' required placeholder='Confirm Password'/>
                 </div>
+                
+                {signuperror.usernameError ? <div style={{color:'red', fontSize: 20, marginLeft:'5%', marginRight:"5%", textAlign:'center'}}><span ><ion-icon name="alert-circle-outline"></ion-icon></span> {signuperror.usernameError}</div> : ""}
+                {signuperror.emailError ? <div style={{color:'red', fontSize: 20, marginLeft:'5%', marginRight:"5%", textAlign:'center'}}><span ><ion-icon name="alert-circle-outline"></ion-icon></span> {signuperror.emailError}</div> : ""}
+                {signuperror.passwordError ? <div style={{color:'red', fontSize: 20, marginLeft:'5%', marginRight:"5%", textAlign:'center'}}><span ><ion-icon name="alert-circle-outline"></ion-icon></span> {signuperror.passwordError}</div> : ""}
+
                 <button  type="submit">SIGN UP</button>
-            </form>
-            
-            <p>By signing up, I agree to your <b> Terms of Service </b>and <b>Privacy Policy</b>.</p>
-            
-        </div>
-        {/* <div>
-    {
-        allEntry.map((a) => {
-            return(<div>
-                <p>{a.fullname}</p>
-                <p>{a.email}</p>
-                <p>{a.password}</p>
-            </div>)
-        })
-
-    }
-    </div> */}
+            </form>                        
+            <p>By signing up, I agree to your <b> Terms of Service </b>and <b>Privacy Policy</b>.</p>                        
+        </div>    
     </div>
-    
-    
-
     </>
   )
 }

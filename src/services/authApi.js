438ecca6811c8a89299d459f12f63d15d3153f7b
anshRS <anshraj0426@gmail.com>
2023-01-10 16:30:01 +0530

    Modified the endpoint service for SignUp component

--- src/services/authApi.js
+++ src/services/authApi.js
@@ -2,14 +2,14 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 export const authApi = createApi({
     reducerPath: 'authApi',
-    baseQuery: fetchBaseQuery({ baseUrl: 'http://127.0.0.1:8000/api/' }),
+    baseQuery: fetchBaseQuery({ baseUrl: 'http://127.0.0.1:8000/api/genexp/' }),
     endpoints: (builder) => ({
       signup: builder.mutation({
         query: (user) => {
-            console.log(user);
+            
             return{
                 
-                url:'genexp/signup/',
+                url:'signup/',
                 method: 'POST',
                 body: user,
                 headers: {

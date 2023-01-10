35cf2b428ad354ccad9c9bb74c8aeca3ce841d81
anshRS <anshraj0426@gmail.com>
2023-01-10 16:28:31 +0530

    Updated endpoints of API for signup

--- genexpBackend/genexpAPI/urls.py
+++ genexpBackend/genexpAPI/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from genexpAPI.views import *
 
 urlpatterns = [
-    path('genexp/signup/',SignupView.as_view(),name='signup'),
+    path('signup/',SignupView.as_view(),name='signup'),
 
 ]
